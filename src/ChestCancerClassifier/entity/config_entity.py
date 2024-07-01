915e43af521626de6c281a4294af5644cd9e696d
KUNALKANK1737 <kankkunal007@gmail.com>
2024-07-01 13:19:49 +0530

    Model Evaluation Done

--- src/ChestCancerClassifier/entity/config_entity.py
+++ src/ChestCancerClassifier/entity/config_entity.py
@@ -21,7 +21,10 @@ class PrepareBaseModelConfig:
     params_include_top: bool
     params_weights: str
     params_classes: int
-    
+
+
+
+
 @dataclass(frozen=True)
 class TrainingConfig:
     root_dir: Path
@@ -32,3 +35,14 @@ class TrainingConfig:
     params_batch_size: int
     params_is_augmentation: bool
     params_image_size: list
+
+
+
+@dataclass(frozen=True)
+class EvaluationConfig:
+    path_of_model: Path
+    training_data: Path
+    all_params: dict
+    mlflow_uri: str
+    params_image_size: list
+    params_batch_size: int
